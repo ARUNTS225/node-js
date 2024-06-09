9c840c56ceecbe834709f6e1f080b1fc67300408
ARUNTS225 <arun220420025@gmail.com>
2024-06-09 12:19:20 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const fs=require('fs');
+const PORT = process.env.PORT || 8000
 try{
     app.get('/',(req,res)=>{
     let datetime = new Date()
     fs.writeFileSync('datetime.txt', datetime.toString())
     let data= fs.readFileSync('datetime.txt','utf-8')
-    res.send(data)})
-app.listen(8000)
+    res.send(data + '<h1>created by Arun</h1>')})
+app.listen(PORT)
 }catch(err)
 { console.log(err)}
 
